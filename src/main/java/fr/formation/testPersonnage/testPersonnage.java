28cb33555968d4530e01ba668ce59d24e05570a4
Anh-Khoa Huynh <khoakun@gmail.com>
2021-05-04 14:34:22 +0200

    push test

--- src/main/java/fr/formation/testPersonnage/testPersonnage.java
+++ src/main/java/fr/formation/testPersonnage/testPersonnage.java
@@ -23,6 +23,11 @@ public class testPersonnage {
 		toSend = C.demander(alfred, equipeA, equipeB);
 		
 		System.out.println(toSend[0]+" "+toSend[1]);
+
 		System.out.println("Salut ! ");
+
+		
+		// Push test
+
 	}
 }
