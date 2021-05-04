bcf006c7c9c6bf61a34b081d7f79284855eecca6
BaptisteCotte <baptiste.cotte.17@gmail.com>
2021-05-04 14:13:04 +0200

    ajout du petit main de test

---
+++ src/main/java/fr/formation/testPersonnage/testPersonnage.java
@@ -0,0 +1,27 @@
+package fr.formation.testPersonnage;
+
+import fr.formation.personnage.*;
+
+public class testPersonnage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Personnage bob = new Knight("Bob",25,Race.HUMAN);
+		Personnage paul = new Sorcerer("Paul",25,Race.ELF);
+		Personnage alfred = new Priest("Alfred",20,Race.DWARF);
+		Equipe equipeA = new Equipe(bob,paul,alfred);
+		
+		Personnage boby = new Knight("Boby",25,Race.HUMAN);
+		Personnage pauly = new Sorcerer("Pauly",25,Race.ELF);
+		Personnage alfredy = new Priest("Alfredy",20,Race.DWARF);
+		Equipe equipeB = new Equipe(boby,pauly,alfredy);
+		
+		Combat C = new Combat(equipeA,equipeB);
+		int toSend[] = {0,0};
+		
+		
+		toSend = C.demander(alfred, equipeA, equipeB);
+		
+		System.out.println(toSend[0]+" "+toSend[1]);
+	}
+}
