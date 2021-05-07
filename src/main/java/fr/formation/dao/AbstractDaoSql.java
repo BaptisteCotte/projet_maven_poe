d3cbbf54c5df5eb6b2a8a3cd5e83127a4edb32e2
Anh-Khoa Huynh <khoakun@gmail.com>
2021-05-07 09:33:02 +0200

    Connexion à la bdd

--- src/main/java/fr/formation/dao/AbstractDaoSql.java
+++ src/main/java/fr/formation/dao/AbstractDaoSql.java
@@ -1,5 +1,56 @@
 package fr.formation.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public abstract class AbstractDaoSql {
+	
+	private static Connection connection;
+
+
+public AbstractDaoSql() {
+	
+	this.createConnection();
+}
+
+
+public void createConnection () {
+	
+	// Connexion à la bdd 
+	if(connection == null) {
+		
+	try {
+		
+		// Chargement du pilote
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+	} catch (ClassNotFoundException cne) {
+	
+		System.out.println("JDBC Driver can't be loaded ...");
+		cne.printStackTrace();
+		
+	}
+	
+	try {
+		
+		System.out.println("Attempting to connect ...");
+		
+		// Connexion au serveur
+		connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bddprojet?serverTimezone=UTC", "UserJeu", "UserJeu1234");
+		
+	} catch (SQLException sqle) {
+		
+		System.out.println("Database connection failed ...");
+		sqle.printStackTrace();
+		
+	}
+	
+}
+	
+}
+	
+
+	
 
 }
