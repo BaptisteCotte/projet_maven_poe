a9763bfd873610fef1071f8da31048cb5877a865
BaptisteCotte <baptiste.cotte.17@gmail.com>
2021-05-07 14:52:35 +0200

    Ajout de update(knight) dans knightDao

--- src/main/java/fr/formation/dao/KnightDaoSql.java
+++ src/main/java/fr/formation/dao/KnightDaoSql.java
@@ -18,18 +18,18 @@ public class KnightDaoSql extends AbstractDaoSql implements IKnightDao {
 
 		
 		try {
-			ResultSet ResultSet = this.getResult("SELECT * FROM personnage INNER JOIN knight ON personnage.PER_ID = knight.KNI_PER_ID;");
+			ResultSet resultSet = this.getResult("SELECT * FROM personnage INNER JOIN knight ON personnage.PER_ID = knight.KNI_PER_ID;");
 
 			
-			while (ResultSet.next()) {
-				Knight knight = new Knight(ResultSet.getString("PER_NOM"),ResultSet.getInt("PER_AGE"),Race.valueOf(Race.class,ResultSet.getString("PER_RACE")));
-				knight.setId(ResultSet.getInt("PER_ID"));
-				knight.setLvl(ResultSet.getInt("PER_LEVEL"));
-				knight.setXp(ResultSet.getInt("PER_XP"));
-				knight.setHp(ResultSet.getInt("PER_HP"));
-				knight.setMaxHp(ResultSet.getInt("PER_MAXHP"));
-				knight.setBaseDmg(ResultSet.getInt("PER_BASEDMG"));
-				knight.setState(ResultSet.getBoolean("PER_STATE"));
+			while (resultSet.next()) {
+				Knight knight = new Knight(resultSet.getString("PER_NOM"),resultSet.getInt("PER_AGE"),Race.valueOf(Race.class,resultSet.getString("PER_RACE")));
+				knight.setId(resultSet.getInt("PER_ID"));
+				knight.setLvl(resultSet.getInt("PER_LEVEL"));
+				knight.setXp(resultSet.getInt("PER_XP"));
+				knight.setHp(resultSet.getInt("PER_HP"));
+				knight.setMaxHp(resultSet.getInt("PER_MAXHP"));
+				knight.setBaseDmg(resultSet.getInt("PER_BASEDMG"));
+				knight.setState(resultSet.getBoolean("PER_STATE"));
 				knights.add(knight);
 			}
 		}
@@ -44,18 +44,18 @@ public class KnightDaoSql extends AbstractDaoSql implements IKnightDao {
 	public Optional<Knight> getById(int id) {
 		try {
 			//Selectionner tous les produits
-			ResultSet ResultSet = this.getResult("SELECT * FROM personnage INNER JOIN knight ON personnage.PER_ID = knight.KNI_PER_ID WHERE PER_ID = " + id);
+			ResultSet resultSet = this.getResult("SELECT * FROM personnage INNER JOIN knight ON personnage.PER_ID = knight.KNI_PER_ID WHERE PER_ID = " + id);
 			
 			//Parcours du r�sultat
-			if (ResultSet.next()) {
-				Knight knight = new Knight(ResultSet.getString("PER_NOM"),ResultSet.getInt("PER_AGE"),Race.valueOf(Race.class,ResultSet.getString("PER_RACE")));
-				knight.setId(ResultSet.getInt("PER_ID"));
-				knight.setLvl(ResultSet.getInt("PER_LEVEL"));
-				knight.setXp(ResultSet.getInt("PER_XP"));
-				knight.setHp(ResultSet.getInt("PER_HP"));
-				knight.setMaxHp(ResultSet.getInt("PER_MAXHP"));
-				knight.setBaseDmg(ResultSet.getInt("PER_BASEDMG"));
-				knight.setState(ResultSet.getBoolean("PER_STATE"));
+			if (resultSet.next()) {
+				Knight knight = new Knight(resultSet.getString("PER_NOM"),resultSet.getInt("PER_AGE"),Race.valueOf(Race.class,resultSet.getString("PER_RACE")));
+				knight.setId(resultSet.getInt("PER_ID"));
+				knight.setLvl(resultSet.getInt("PER_LEVEL"));
+				knight.setXp(resultSet.getInt("PER_XP"));
+				knight.setHp(resultSet.getInt("PER_HP"));
+				knight.setMaxHp(resultSet.getInt("PER_MAXHP"));
+				knight.setBaseDmg(resultSet.getInt("PER_BASEDMG"));
+				knight.setState(resultSet.getBoolean("PER_STATE"));
 				return Optional.of(knight);
 			}
 		}
@@ -71,10 +71,10 @@ public class KnightDaoSql extends AbstractDaoSql implements IKnightDao {
 		try {
 			PreparedStatement insertPersonnage = connection.prepareStatement("INSERT INTO personnage (PER_NOM,PER_AGE,PER_RACE,PER_LEVEL,PER_XP,PER_HP,PER_MAXHP,PER_BASEDMG,PER_STATE) VALUES ("+entity.getName()+","+entity.getAge()+","+entity.getRace().toString()+","+entity.getLvl()+","+entity.getXp()+","+entity.getHp()+","+entity.getMaxHp()+","+entity.getBaseDmg()+","+entity.isState()+");");
 			insertPersonnage.execute();
-			ResultSet ResultSet = this.getResult("SELECT PER_ID FROM personnage ORDER BY PER_ID DESC LIMIT 1;");
+			ResultSet resultSet = this.getResult("SELECT PER_ID FROM personnage ORDER BY PER_ID DESC LIMIT 1;");
 			
-			while (ResultSet.next()) {
-				int id = ResultSet.getInt("PER_ID");
+			while (resultSet.next()) {
+				int id = resultSet.getInt("PER_ID");
 				PreparedStatement insertKnight = connection.prepareStatement("INSERT INTO knight (KNI_PER_ID,KNI_ARMOR) VALUES ("+id+","+entity.getArmor()+")");
 				insertKnight.execute();
 			}	
@@ -86,7 +86,16 @@ public class KnightDaoSql extends AbstractDaoSql implements IKnightDao {
 
 	@Override
 	public void update(Knight entity) {
-		// TODO Auto-generated method stub
+		
+		try {
+			PreparedStatement updatePersonnage = connection.prepareStatement("UPDATE personnage SET PER_NOM = "+entity.getName()+", PER_AGE = "+entity.getAge()+", PER_RACE = "+entity.getRace().toString()+", PER_LEVEL = "+entity.getLvl()+", PER_XP = "+entity.getXp()+", PER_HP = "+entity.getHp()+", PER_MAXHP = "+entity.getMaxHp()+", PER_BASEDMG = "+entity.getBaseDmg()+", PER_STATE = "+entity.isState()+" WHERE (PER_ID = "+entity.getId()+");");
+			PreparedStatement updateKnight = connection.prepareStatement("UPDATE knight SET KNI_ARMOR = "+entity.getArmor()+" WHERE (KNI_PER_ID = "+entity.getId()+");");	
+			updatePersonnage.execute();
+			updateKnight.execute();
+		}
+		catch (SQLException sqle) {
+			sqle.printStackTrace();
+		}
 
 	}
 
