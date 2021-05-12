d551a9190267a12d8ec72d05c7503cabb5de8459
Anh-Khoa Huynh <khoakun@gmail.com>
2021-05-12 09:30:21 +0200

    random commit

--- src/main/java/fr/formation/model/Sorcerer.java
+++ src/main/java/fr/formation/model/Sorcerer.java
@@ -7,78 +7,79 @@ import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
-@Table(name="sorcerer")
-@PrimaryKeyJoinColumn(name="SOR_PER_ID", referencedColumnName = "PER_ID")
+@Table(name = "sorcerer")
+@PrimaryKeyJoinColumn(name = "SOR_PER_ID", referencedColumnName = "PER_ID")
 public class Sorcerer extends Personnage implements IMage {
 
-	//Attributs
-	@Column(name="SOR_MANA", nullable = false)
+	// Attributs
+	@Column(name = "SOR_MANA", nullable = false)
 	@NotNull
 	protected int mana = 100;
-	
-	@Column(name="SOR_MAXMANA", nullable = false)
+
+	@Column(name = "SOR_MAXMANA", nullable = false)
 	@NotNull
 	protected int maxMana = 100;
-	
-	//Constructeur
-	public Sorcerer(String name,int age,Race race) {
-		super(name,age,race);
+
+	// Constructeur
+	public Sorcerer(String name, int age, Race race) {
+		super(name, age, race);
 		this.maxMana = defineMaxMana(race);
 		this.mana = this.maxMana;
 	}
-	
-	//Accesseurs
+
+	// Accesseurs
 	public int getMana() {
 		return mana;
 	}
-	public void setMana(int mana) {
+
+	public void setMana(int mana) { 
 		this.mana = mana;
 	}
-	
+
 	public int getMaxMana() {
 		return maxMana;
 	}
+
 	public void setMaxMana(int maxMana) {
 		this.maxMana = maxMana;
 	}
-	
-	
-	//Methodes
+
+	// Methodes
 	@Override
 	public int useMagic() {
-		if(this.getMana() >= 10) {
-			this.setMana(this.getMana()-10);
-			System.out.println(this.getName()+" a "+this.getMana()+" mana");
-			return(this.getLvl());
+		if (this.getMana() >= 10) {
+			this.setMana(this.getMana() - 10);
+			System.out.println(this.getName() + " a " + this.getMana() + " mana");
+			return (this.getLvl());
 		}
 		return 0;
 	}
-	
+
 	public int defineMaxMana(Race race) {
-		switch(race) {
-		case ELF :
+		switch (race) {
+		case ELF:
 			return 120;
-		default :
+		default:
 			return 100;
 		}
 	}
-	
-	public void attaquer(Personnage p){
-		System.out.println(this.getName()+" attaque "+p.getName()+" !");
-		p.etreAttaque(this.getBaseDmg()+useMagic(), this);
+
+	public void attaquer(Personnage p) {
+		System.out.println(this.getName() + " attaque " + p.getName() + " !");
+		p.etreAttaque(this.getBaseDmg() + useMagic(), this);
 	}
-	
+
 	public void lvlUp() {
-		if(this.getXp()>=(this.getLvl()*200)) {
-			this.setXp(this.getXp()-(this.getLvl()*200));
-			this.setLvl(this.getLvl()+1);
-			System.out.println(this.getName()+" gagne un niveau !");
-			this.setMaxHp(this.getMaxHp()+10);
-			this.setBaseDmg(this.getBaseDmg()+1);
-			this.setMaxMana(this.getMaxMana()+10);
+		if (this.getXp() >= (this.getLvl() * 200)) {
+			this.setXp(this.getXp() - (this.getLvl() * 200));
+			this.setLvl(this.getLvl() + 1);
+			System.out.println(this.getName() + " gagne un niveau !");
+			this.setMaxHp(this.getMaxHp() + 10);
+			this.setBaseDmg(this.getBaseDmg() + 1);
+			this.setMaxMana(this.getMaxMana() + 10);
 		}
 	}
-	
+
 	public void regen() {
 		this.setHp(this.getMaxHp());
 		this.setMana(this.getMaxMana());
@@ -90,6 +91,5 @@ public class Sorcerer extends Personnage implements IMage {
 				+ ", lvl=" + lvl + ", xp=" + xp + ", hp=" + hp + ", maxHp=" + maxHp + ", baseDmg=" + baseDmg
 				+ ", state=" + state + "]";
 	}
-	
-	
+
 }
